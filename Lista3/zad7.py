85090beaa1771e2fc99d70b1d0205c5c81b7cc9e
alypko <artem2310@hotmail.com>
2020-11-09 00:49:31 +0100

    modified zad7

--- Lista3/zad7.py
+++ Lista3/zad7.py
@@ -1,12 +1,12 @@
 N = int(input("Wprowadz liczbe: "))
 a = 0
 b = 1
+print(a)
+print(b)
 while True:
-    print(a)
     Sum = a + b
-    b = Sum
-    a = Sum + a
-    #print(b)
     print(Sum)
-    if Sum > N:
+    a = b
+    b = Sum
+    if Sum > N-2:
         break
