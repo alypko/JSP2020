5c5e3f444875fc4002fa849c219d7b627cf3379f
alypko <artem2310@hotmail.com>
2020-11-12 08:56:12 +0100

    update zad6

--- Lista4/zad6.py
+++ Lista4/zad6.py
@@ -1,6 +1,5 @@
 
-# Python3 program change RGB Color 
-# Model to HSV Color Model 
+
   
 def rgbtohsv(r, g, b): 
   
@@ -8,27 +7,27 @@ def rgbtohsv(r, g, b):
     g = g / 255.0
     b = b / 255.0
   
-    maks = max(r, g, b)    # maximum of r, g, b 
-    Min = min(r, g, b)    # minimum of r, g, b 
-    delta = maks - Min       # diff of cmax and cmin. 
+    maks = max(r, g, b)    
+    Min = min(r, g, b)    
+    delta = maks - Min       
   
     
     if maks == Min:  
         h = 0
       
-    # if cmax equal r then compute h 
+    
     elif maks == r:  
         h = (60 * ((g - b) / delta) + 360) % 360
   
-    # if cmax equal g then compute h 
+    
     elif maks == g: 
         h = (60 * ((b - r) / delta) + 120) % 360
   
-    # if cmax equal b then compute h 
+ 
     elif maks == b: 
         h = (60 * ((r - g) / delta) + 240) % 360
   
-    # if cmax equal zero 
+
     if maks == 0: 
         s = 0
     else: 
