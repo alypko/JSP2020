00c51ac8295902857a4fa3b2547af866529871e3
alypko <artem2310@hotmail.com>
2020-12-17 09:36:16 +0100

    modified zad2, zad3 from lista7

--- Lista7/zad2.py
+++ Lista7/zad2.py
@@ -2,6 +2,7 @@ import random
 import time 
 
 def ins_sort(a):
+    x1 = time.time()
     n = len(a)
     for i in range(1, n):
         key = a[i]
@@ -10,10 +11,11 @@ def ins_sort(a):
             a[j+1] = a[j]
             j-=1
         a[j+1]=key
-    return a
+    x2= time.time()
+    print(a,x2-x1)
 
 
 
 l = [random.randint(0,20) for i in range(100)]
-print(ins_sort(l), time.process_time())
+ins_sort(l)
 
--- Lista7/zad3.py
+++ Lista7/zad3.py
@@ -2,11 +2,13 @@ import random
 import time 
 
 def bubbleSort(a):
+    x1=time.time()
     n = len(a)
     for i in range(n):
         for j in range(0, n-i-1):
             if(a[j]>a[j+1]):
                 a[j], a[j+1] = a[j+1], a[j]
-    return a
+    x2=time.time()
+    print(a, x2-x1)
 l = [random.randint(0,20) for i in range(500)]
-print(bubbleSort(l), time.process_time())
+bubbleSort(l)
