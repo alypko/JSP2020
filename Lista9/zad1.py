139d84fff19ef94477fa62990b4a3ae095ce23b2
alypko <artem2310@hotmail.com>
2021-01-14 09:11:37 +0100

    zad1.py

--- Lista9/zad1.py
+++ Lista9/zad1.py
@@ -3,4 +3,4 @@ import numpy as np
 A = np.array([[1,2,3,-2,-1],[3,5,5,-3,-9],[2,3,2,0,-8],[2,6,7,-5,1],[1,2,6,-4,-10]])
 B = np.array([6,2,-5,17,12])
 odp = np.linalg.inv(A).dot(B)
-print("x=",odp[0]," y=",opd[1]," z=",odp[2], " t=",odp[3]," u=",odp[4])
+print("x=",odp[0]," y=",odp[1]," z=",odp[2], " t=",odp[3]," u=",odp[4])
